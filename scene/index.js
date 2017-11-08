1669b381c1cbfd1cecec160c890f9cf671ac2ea2
MagnusLin <magnuslin@vanchu.net>
2017-11-08 20:07:00 +0800

    work on

--- scene/index.js
+++ scene/index.js
@@ -2,7 +2,9 @@ const assert = require('assert');
 const StepDescriber = require('../step/describer');
 const StepExecuter = require('../step/executer');
 
-module.exports = class {
+let topScene = undefined;
+
+module.exports = class Scene{
     constructor(desc) {
         this.executer = StepExecuter.create();
         this.desc = desc;
@@ -20,39 +22,58 @@ module.exports = class {
         return allSteps.map(step => step.timeout).reduce((total, current) => total + current, 0);
     }
 
-    get lastStep() {
-        return this.stepsBeforeCurrent[this.stepsBeforeCurrent.length-1];
+    _lastStep() {
+        return this._stepsBeforeCurrent()[this._stepsBeforeCurrent().length-1];
     }
 
-    get stepsBeforeCurrent() {
-        
-        let allSteps = this._subScenes.reduce((allSteps, currentScene) => allSteps.concat(currentScene.stepsBeforeCurrent), []);
+    _stepsBeforeCurrent() {
+        let allSteps = this._subScenes.reduce((allSteps, currentScene) => allSteps.concat(currentScene._stepsBeforeCurrent()), []);
         allSteps = allSteps.concat(this._completedSteps);
         return allSteps;
     }
 
+    get lastStep() {
+        return topScene._lastStep();
+    }
+
+    get stepsBeforeCurrent() {
+        return topScene._stepsBeforeCurrent();
+    }
+
     addStep(step) {
-        let stepDesc = new StepDescriber(step, this._context);
+        let stepDesc = new StepDescriber(step);
         this._allSteps.push(stepDesc);
     }
 
     loadScene(scene) {
-        this._subScenes.push(scene);
+        assert(scene instanceof Scene, 'Scene.loadScene(): expect param tobe instanceof Scene.');
+        this._subScenes.push(scene.clone());
     }
 
-    async runSubScenes() {
+    async runSubScenes(top = true) {
+        if(top) topScene = this;
         for(let subScene of this._subScenes) {
             subScene.executer = this.executer;
-            await subScene.runSubScenes();
-            await subScene.run();
+            subScene._parent = this;
+            await subScene.runSubScenes(false);
+            await subScene.run(false);
         }
     }
 
-    async run() {
+    async run(top = true) {
+        if(top) topScene = this;
         for(let step of this._allSteps) {
             await step.exec(this.executer);
             this._completedSteps.push(step);
         }
+    }
+
+    clone() {
+        let newScene = new Scene(this.desc);
+        newScene._allSteps = this._allSteps.map(step => step.clone());
+        newScene._subScenes = this._subScenes.map(scene => scene.clone());
+        newScene._completedSteps = [];
+        return newScene;
     }
 }
 
--- step/describer.js
+++ step/describer.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-module.exports = class {
+module.exports = class Describer{
     constructor({name, timeout = 2000, prepareRequest, handleResponse}) {
         assert(typeof name === 'string', 'StepDescriber: expect name to be a string.');
         assert(typeof prepareRequest === 'function', 'StepDescriber: expect prepareRequest to be a function.');
@@ -24,6 +24,14 @@ module.exports = class {
         }
         await this._handleResponse(this.error, this.response, executer).catch(err => {
             throw new Error(`[${this.name}] ${err.message}`);
+        });
+    }
+    clone() {
+        return new Describer({
+            name: this.name, 
+            timeout: this.timeout, 
+            prepareRequest: this._prepareRequest, 
+            handleResponse: this._handleResponse
         });
     }
 }
